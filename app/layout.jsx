1e9b15c89d27b69beafa994449b8faae5cb29cf2
Tristan Jake <tristan@example.com>
2026-08-04 18:35:47 +0800

    Add noindex for testing

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="robots" content="noindex, nofollow" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
